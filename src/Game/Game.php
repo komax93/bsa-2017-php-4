77e27442f41fb5fde30bfc9c2fe337d97db403d5
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-10 00:45:51 +0300

    added comments to Game.php

--- src/Game/Game.php
+++ src/Game/Game.php
@@ -11,11 +11,18 @@ class Game
 
     private $app;
 
+    /**
+     * Game constructor.
+     */
     public function __construct()
     {
         $this->app = new GameServiceProvider(new Castle(), new User(0, self::COINS_TO_WIN));
     }
 
+    /**
+     * @param Reader $reader
+     * @param Writer $writer
+     */
     public function start(Reader $reader, Writer $writer): void
     {
         $writer->writeln("Welcome to the game. Enter your command!");
@@ -26,6 +33,10 @@ class Game
         }
     }
 
+    /**
+     * @param Reader $reader
+     * @param Writer $writer
+     */
     public function run(Reader $reader, Writer $writer)
     {
         $input = trim($reader->read());
