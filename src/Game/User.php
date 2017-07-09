64cb8ff533eedad360e72097de4f07c1f6cb2266
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-10 00:35:03 +0300

    added comments to User.php

--- src/Game/User.php
+++ src/Game/User.php
@@ -13,17 +13,31 @@ class User
     private $coinsCount;
     private $maxCoinsCount;
 
+    /**
+     * User constructor.
+     * @param int $coinsCount
+     * @param $maxCoinsCount
+     */
     public function __construct($coinsCount = 0, $maxCoinsCount)
     {
         $this->coinsCount = 0;
         $this->maxCoinsCount = $maxCoinsCount;
     }
 
+    /**
+     * This method return users coins count
+     * @return int
+     */
     public function getCoinsCount()
     {
         return $this->coinsCount;
     }
 
+    /**
+     * This method add coins to user
+     * @param $count
+     * @return string
+     */
     public function addCoins($count)
     {
         $this->coinsCount += $count;
@@ -37,11 +51,19 @@ class User
         }
     }
 
+    /**
+     * This method return max coins count, which user can collect
+     * @return mixed
+     */
     public function getMaxCoinsCount()
     {
         return $this->maxCoinsCount;
     }
 
+    /**
+     * This method set max coins count, which user can collect
+     * @param $maxCoinsCount
+     */
     public function setMaxCoinsCount($maxCoinsCount)
     {
         $this->maxCoinsCount = $maxCoinsCount;
