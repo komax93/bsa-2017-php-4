d5fdd254d35ae84523c043f13f5570e277629cb6
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-09 19:36:55 +0300

    removed test_commands from Command.php

--- src/Game/Command.php
+++ src/Game/Command.php
@@ -15,33 +15,6 @@ use BinaryStudioAcademy\Game\Exceptions\WrongCommandException;
 
 class Command
 {
-    const TEST_COMMANDS = [
-        ['status', "You're at hall. You have 0 coins."],
-        ['observe', "There 1 coin(s) here."],
-        ['grab', "Congrats! Coin has been added to inventory."],
-        ['status', "You're at hall. You have 1 coins."],
-        ['grab', "There is no coins left here. Type 'where' to go to another location."],
-        ['where', "You're at hall. You can go to: basement, corridor."],
-        ['go basement', "You're at basement. You can go to: cabinet, hall."],
-        ['observe', "There 2 coin(s) here."],
-        ['grab', "Congrats! Coin has been added to inventory."],
-        ['grab', "Congrats! Coin has been added to inventory."],
-        ['grab', "There is no coins left here. Type 'where' to go to another location."],
-        ['status', "You're at basement. You have 3 coins."],
-        ['where', "You're at basement. You can go to: cabinet, hall."],
-        ['go tower', "Can not go to tower."],
-        ['go cabinet', "You're at cabinet. You can go to: corridor."],
-        ['observe', "There 1 coin(s) here."],
-        ['collect', "Unknown command: 'collect'."],
-        ['grab', "Congrats! Coin has been added to inventory."],
-        ['status', "You're at cabinet. You have 4 coins."],
-        ['go corridor', "You're at corridor. You can go to: hall, cabinet, bedroom."],
-        ['grab', "There is no coins left here. Type 'where' to go to another location."],
-        ['go bedroom', "You're at bedroom. You can go to: corridor."],
-        ['observe', "There 1 coin(s) here."],
-        ['grab', "Good job. You've completed this quest. Bye!"]
-    ];
-
     private $command;
     private $building;
     private $user;
