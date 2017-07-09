859cbfe1b11cbce97c010eec5ac69633c1383637
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-10 00:48:15 +0300

    added comments to RoomsTrait.php

--- src/Game/Traits/RoomsTrait.php
+++ src/Game/Traits/RoomsTrait.php
@@ -11,11 +11,22 @@ namespace BinaryStudioAcademy\Game\Traits;
 
 trait RoomsTrait
 {
+    /**
+     * This method return string with nearest rooms.
+     *
+     * @return string
+     */
     public function getNearestRooms(): string
     {
         return implode(", ", self::NEAREST_ROOMS);
     }
 
+    /**
+     * This method check nearest rooms for current room.
+     *
+     * @param $newRoomName
+     * @return bool
+     */
     public function isNearestRoom($newRoomName): bool
     {
         if (in_array($newRoomName, self::NEAREST_ROOMS)) {
